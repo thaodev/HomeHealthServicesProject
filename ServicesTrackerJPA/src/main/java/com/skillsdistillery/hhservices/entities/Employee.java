2c1290ee81fa67396a3f7cddea75261975e5e9d2
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-05 22:43:19 -0600

    Add Employee entity

---
+++ ServicesTrackerJPA/src/main/java/com/skillsdistillery/hhservices/entities/Employee.java
@@ -0,0 +1,60 @@
+package com.skillsdistillery.hhservices.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String address;
+
+	public Employee() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+
+}
