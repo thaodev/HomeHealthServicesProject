fe61fc115831f1c3a8add70f545fc71336576bee
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-18 10:10:19 -0600

    complete symbol sort pipe

--- ngServiceTracker/src/app/models/schedule.ts
+++ ngServiceTracker/src/app/models/schedule.ts
@@ -1,2 +1,26 @@
 export class Schedule {
+  id: number;
+  date: string;
+  billAmount: number;
+  payrollAmount: number;
+  scheduleStatus: string;
+  billStatus: string;
+  payrollStatus: string;
+
+  constructor( id: number,
+    date: string,
+    billAmount: number,
+    payrollAmount: number,
+    scheduleStatus: string,
+    billStatus: string,
+    payrollStatus: string) {
+      this.id = id;
+      this.date = date;
+      this.billAmount = billAmount;
+      this.payrollAmount = payrollAmount;
+      this.scheduleStatus = scheduleStatus;
+      this.billStatus = billStatus;
+      this.payrollStatus = payrollStatus;
+    }
+
 }
